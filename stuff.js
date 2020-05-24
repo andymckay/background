5df6d26a32e99a1a8f1112b4034eb6ac9eeaf2f4
Andy McKay <andymckay@github.com>
2020-05-24 12:09:30 -0700

    fix clock

--- stuff.js
+++ stuff.js
@@ -1,7 +1,15 @@
 function updateClock() {
     let elem = document.getElementById("clock");
     let date = new Date();
-    elem.innerText = `${date.getHours()}:${date.getMinutes()}`;
+    let hours = date.getHours();
+    let ampm = "AM"
+    if ((hours) > 12) {
+        hours = hours - 12;
+        ampm = "PM"
+    }
+    let minutes = date.getMinutes();
+    minutes = (minutes < 10 ? '0':'') + minutes;
+    elem.innerText = `${hours}:${minutes} ${ampm}`;
     window.setInterval(updateClock, 3000);
 }
 
